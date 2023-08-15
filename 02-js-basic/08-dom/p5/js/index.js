a606df228dc3eea462d4f4ea37a8a105a79f6188
dongkaiqi <kaiqi.dong@learnable.ai>
2023-08-15 14:53:14 +0800

    feat: 02 jsbasic dom

--- 02-js-basic/08-dom/p5/js/index.js
+++ 02-js-basic/08-dom/p5/js/index.js
@@ -2,21 +2,25 @@
 var listDom = document.querySelector(".todo-list");
 var input = document.querySelector(".txt");
 
+function addTask(value) {
+  var li = document.createElement("li");
+  var span = document.createElement("span");
+  var button = document.createElement("button");
+  span.innerText = value;
+  button.innerText = "删除";
+  // 点击删除后，删除对应的待办事项
+  button.addEventListener("click", function () {
+    li.remove();
+  });
+  li.appendChild(span);
+  li.appendChild(button);
+  listDom.appendChild(li);
+}
+
 input.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
-    if (this.value) {
-      var li = document.createElement("li");
-      var span = document.createElement("span");
-      var button = document.createElement("button");
-      span.innerText = this.value;
-      button.innerText = "删除";
-      // 点击删除后，删除对应的待办事项
-      button.addEventListener("click", function () {
-        li.remove();
-      });
-      li.appendChild(span);
-      li.appendChild(button);
-      listDom.appendChild(li);
+    if (this.value.trim()) {
+      addTask(this.value.trim());
       this.value = "";
     }
   }
